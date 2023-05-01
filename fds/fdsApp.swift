49bd53512147fefe5913df7761f5fab334e5a45d
tabber <tabber@jejememe.com>
2023-05-02 08:32:51 +0900

    Initial Commit

---
+++ fds/fdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fdsApp.swift
+//  fds
+//
+//  Created by Tabber on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct fdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
